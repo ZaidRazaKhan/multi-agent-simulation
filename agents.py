ceb4b844c808106ec9619b6da541de3bdcfb89c2
zaidrazakhan <zaid.kv5@gmail.com>
2020-07-26 00:59:07 +0530

    Completed 3 agents logic code

--- agents.py
+++ agents.py
@@ -2,12 +2,14 @@ from pade.core.agent import Agent
 from pade.misc.utility import display_message, start_loop, call_later
 from pade.acl.messages import ACLMessage
 from pade.acl.aid import AID
+from protocols import *
+from behavious import *
 
 
 class GoalKeeper(Agent):
     def __init__(self, aid):
         super(GoalKeeper, self).__init__(aid=aid, debug=False)
-        self.message = None
+        self.message = 'HOLA!!'
         print('Goal Keeper')
 
     def on_start(self):
@@ -20,7 +22,7 @@ class GoalKeeper(Agent):
 
     def react(self, message):
         super(GoalKeeper, self).react(message)
-        display_message(self.aid.localname, 'Ball is succesfully passed to {}'.format(message))
+        display_message(self.aid.localname, 'Ball is succesfully passed from {}'.format(message.receivers[0].name))
 
     def set_message(self, message_content):
         message = ACLMessage(ACLMessage.INFORM)
@@ -33,10 +35,46 @@ class BallReceivingWinger(Agent):
     def __init__(self, aid):
         super(BallReceivingWinger, self).__init__(aid=aid, debug=False)
         print('Winger')
+        message = ACLMessage(ACLMessage.SUBSCRIBE)
+        message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
+        # message.add_receiver(AID(BallReceivingWinger))
+        message.set_content("Ball!!")
+        self.call_later(8.0, self.launch_subscriber_protocol, message)
+
+    def launch_subscriber_protocol(self, message):
+        self.protocol = SubscriberProtocol(self, message)
+        self.behaviours.append(self.protocol)
+        self.protocol.on_start()
 
     def react(self, message):
         super(BallReceivingWinger, self).react(message)
         display_message(self.aid.localname, 'Ball received from {}'.format(message.sender.name))
+
+
+
+class Crowd(Agent):
+    
+    def __init__(self, aid, message):
+        super(Crowd, self).__init__(aid)
+
+        self.call_later(8.0, self.launch_subscriber_protocol, message)
+
+    def launch_subscriber_protocol(self, message):
+        self.protocol = SubscriberProtocol(self, message)
+        self.behaviours.append(self.protocol)
+        self.protocol.on_start()
+
+
+class AgentPublisher(Agent):
+
+    def __init__(self, aid):
+        super(AgentPublisher, self).__init__(aid)
+
+        self.protocol = PublisherProtocol(self)
+        self.timed = Time(self, self.protocol.notify)
+
+        self.behaviours.append(self.protocol)
+        self.behaviours.append(self.timed)
 
 
 class Defender(Agent):
@@ -47,8 +85,13 @@ class Defender(Agent):
 
 class Striker(Agent):
     def __init__(self, aid):
-        super(Striker, self).__init__(aid=aid, debug=False)
-        print('Striker')
+        super(Striker, self).__init__(aid)
+
+        self.protocol = PublisherProtocol(self)
+        self.timed = Time(self, self.protocol.notify)
+
+        self.behaviours.append(self.protocol)
+        self.behaviours.append(self.timed)
 
 
 class Winger(Agent):
