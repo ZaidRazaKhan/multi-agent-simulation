36d7f889f9e4d70dcc2579515f7e39817a0a1a90
zaidrazakhan <zaid.kv5@gmail.com>
2020-07-26 00:59:29 +0530

    Added behaviour required for the 3 agents

--- behavious.py
+++ behavious.py
@@ -59,31 +59,18 @@ class ComportTemporal(TimedBehaviour):
         super(ComportTemporal, self).on_time()
         self.agent.send(self.message)
 
-# class CompRequest(FipaRequestProtocol):
-#     """FIPA Request Behaviour of the Time agent.
-#     """
-#     def __init__(self, agent):
-#         super(CompRequest, self).__init__(agent=agent,
-#                                           message=None,
-#                                           is_initiator=False)
 
-#     def handle_request(self, message):
-#         super(CompRequest, self).handle_request(message)
-#         display_message(self.agent.aid.localname, 'request message received')
-#         now = datetime.now()
-#         reply = message.create_reply()
-#         reply.set_performative(ACLMessage.INFORM)
-#         reply.set_content(now.strftime('%d/%m/%Y - %H:%M:%S'))
-#         self.agent.send(reply)
+class Time(TimedBehaviour):
 
+    def __init__(self, agent, notify):
+        super(Time, self).__init__(agent, 1)
+        self.notify = notify
+        self.inc = 0
 
-# class CompRequest2(FipaRequestProtocol):
-#     """FIPA Request Behaviour of the Clock agent.
-#     """
-#     def __init__(self, agent, message):
-#         super(CompRequest2, self).__init__(agent=agent,
-#                                            message=message,
-#                                            is_initiator=True)
-
-#     def handle_inform(self, message):
-#         display_message(self.agent.aid.localname, message.content)
+    def on_time(self):
+        super(Time, self).on_time()
+        message = ACLMessage(ACLMessage.INFORM)
+        message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
+        message.set_content(str(random.random()))
+        self.notify(message)
+        self.inc += 0.1
